1ab0066e5937ee81cd2f4afeda04cf264d87bc09
Carlos Rivero <carlosrivero96@gmail.com>
2022-10-20 19:00:39 -0400

    Added buoyancy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] float accelerationSpeed;
     [SerializeField] float maxSpeed;
+    [Header("Rotation")]
     [SerializeField] float rotationSpeed;
+    [SerializeField] float maxRotationTilt;
+    [Header("Buoyancy")]
+    [SerializeField] float buoyancySpeed;
+    [SerializeField] float maxBuoyancy;
+    float buoyancyDirection = 1;
+    static float t = 0f;
+    
     Rigidbody rb;
     float speed;
     float movementVertical;
@@ -39,10 +48,25 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        // Boat rotation
         float rotation = rotationSpeed*movementHorizontal;
-        rb.rotation *= Quaternion.AngleAxis(rotation, transform.up);
-        //rb.rotation = new Quaternion(0, rb.rotation.y + (movementHorizontal*rotationSpeed), 0, 1).normalized;
+        rb.rotation *= Quaternion.AngleAxis(rotation, Vector3.up);
+
+        // Rotation tilt
+        //rb.MoveRotation(Quaternion.Euler(0, 0, movementHorizontal*maxRotationTilt));
+
+        // Boat movement
         speed = Mathf.Clamp(speed + movementVertical, 0, maxSpeed);
         rb.velocity = transform.forward * speed;
+
+        // Boat Buoyancy
+        float buoyancyMovement = Mathf.Lerp(-maxBuoyancy, maxBuoyancy, t) * buoyancyDirection;
+        t += buoyancySpeed * Time.fixedDeltaTime;
+        if (t > 1.0f) {
+            buoyancyDirection *= -1;
+            t = 0f;
+        }
+
+        transform.position = new Vector3(transform.position.x, buoyancyMovement, transform.position.z);
     }
 }
