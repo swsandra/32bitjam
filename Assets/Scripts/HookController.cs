7799f31c1341ce8244697b11c3ecc027338361d2
Sandra <sandrav0609@gmail.com>
2022-10-20 16:12:22 -0400

    add hook controller

--- Assets/Scripts/HookController.cs
+++ Assets/Scripts/HookController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class HookController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    float hookSpeed = 10f;
+    [SerializeField]
+    float camSpeed = 10f;
+    [SerializeField]
+    Camera cam;
+
+    Vector3 screenBounds;
+    float hookWidth;
+
+    float hookTranslation, camTranslation, camXOffset;
+
+    private void Start() {
+        screenBounds = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cam.transform.position.z));
+        camXOffset = cam.transform.position.x;
+        hookWidth = GetComponent<MeshRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        hookTranslation = Input.GetAxisRaw("Horizontal");
+        camTranslation = Input.GetAxisRaw("Vertical");
+
+        Vector3 hookMovement = new Vector3(hookTranslation, 0, 0).normalized * hookSpeed * Time.deltaTime;
+        Vector3 camMovement = new Vector3(0, camTranslation, 0).normalized * camSpeed * Time.deltaTime;
+
+        transform.position += hookMovement;
+        cam.transform.position += camMovement;
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, screenBounds.x+hookWidth, (screenBounds.x * -1)+(camXOffset*2)-hookWidth),
+            transform.position.y,
+            transform.position.z);
     }
 }
