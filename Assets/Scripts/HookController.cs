ae4b497bb90f03d38180a0993bf65d563f5180db
Sandra <sandrav0609@gmail.com>
2022-10-22 20:15:32 -0400

    add start animation

--- Assets/Scripts/HookController.cs
+++ Assets/Scripts/HookController.cs
@@ -11,14 +11,20 @@ public class HookController : MonoBehaviour
     [Header("Vertical Bounds")]
     [SerializeField] Transform seaTop;
     [SerializeField] Transform seaBottom;
+    [SerializeField] GameObject treasure;
     Camera cam;
-    float leftLimit;
-    float rightLimit;
+    float leftLimit, rightLimit, topLimit, bottomLimit;
     float hookWidth, hookHeight;
-    float hookTranslation, camTranslation;
+    float hookTranslation;
+    float camDirection;
+    float initialVerticalSpeed;
+    bool startAnimation;
 
     private void Start() {
+        startAnimation = true;
         cam = Camera.main;
+        camDirection = -1;
+        // Calculate screen limits
         Vector3 screenBounds = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cam.transform.position.z));
         float camXOffset = cam.transform.position.x;
         MeshRenderer renderer = GetComponent<MeshRenderer>();
@@ -26,15 +32,32 @@ public class HookController : MonoBehaviour
         hookHeight = renderer.bounds.size.y;
         leftLimit = screenBounds.x+hookWidth;
         rightLimit = (screenBounds.x*-1)+(camXOffset*2)-hookWidth;
+        topLimit = seaTop.position.y-hookHeight;
+        bottomLimit = treasure.transform.position.y+(hookHeight*2);
+        initialVerticalSpeed = verticalSpeed;
+        // Start camera on sea top
+        cam.transform.position = new Vector3(cam.transform.position.x, topLimit, cam.transform.position.z);
+        // Start hook above camara
+        transform.position = new Vector3(transform.position.x, topLimit+(hookHeight*2), transform.position.z);
     }
 
     void Update()
     {
-        hookTranslation = Input.GetAxisRaw("Horizontal");
-        camTranslation = Input.GetAxisRaw("Vertical");
+        Vector3 hookMovement;
+        if (startAnimation){ // Start animation
+            hookMovement = Vector3.down * (verticalSpeed/2) * Time.deltaTime;
+            transform.position += hookMovement;
+            if (transform.localPosition.y <= 0) startAnimation = false; // Referent to camera
+            return;
+        }
 
-        Vector3 hookMovement = new Vector3(hookTranslation, 0, 0).normalized * horizontalSpeed * Time.deltaTime;
-        Vector3 camMovement = new Vector3(0, camTranslation, 0).normalized * verticalSpeed * Time.deltaTime;
+        bottomLimit = treasure.transform.position.y+(hookHeight*2); // Treasure can change position
+
+        hookTranslation = Input.GetAxisRaw("Horizontal");
+        // camTranslation = Input.GetAxisRaw("Vertical");
+
+        hookMovement = new Vector3(hookTranslation, 0, 0).normalized * horizontalSpeed * Time.deltaTime;
+        Vector3 camMovement = new Vector3(0, camDirection, 0).normalized * verticalSpeed * Time.deltaTime;
 
         transform.position += hookMovement;
         cam.transform.position += camMovement;
@@ -48,7 +71,7 @@ public class HookController : MonoBehaviour
         // Camera movement bounds
         cam.transform.position = new Vector3(
             cam.transform.position.x,
-            Mathf.Clamp(cam.transform.position.y, seaBottom.position.y+(hookHeight*2), seaTop.position.y-hookHeight),
+            Mathf.Clamp(cam.transform.position.y, bottomLimit, topLimit),
             cam.transform.position.z);
     }
 
@@ -57,6 +80,7 @@ public class HookController : MonoBehaviour
             Debug.Log("choco contra un pez");
         }else if (other.CompareTag("Treasure")){
             Debug.Log("toco el tesoro");
+            camDirection = 1;
         }
     }
 }
