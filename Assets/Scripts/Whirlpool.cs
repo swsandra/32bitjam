379ecf131faef8d378a7e894d6de33fa9ccad1d2
Juan Oropeza <jaoc1811@gmail.com>
2022-10-24 22:31:19 -0400

    Added whirlpool

---
+++ Assets/Scripts/Whirlpool.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whirlpool : MonoBehaviour
+{   
+    [SerializeField] float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,speed,0));
+    }
+}
